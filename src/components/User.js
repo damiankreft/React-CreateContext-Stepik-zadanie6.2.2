fce1b108b0b145a11cf14c0427f1c523a4d03e93
damiankreft <25139693+damiankreft@users.noreply.github.com>
2023-06-17 17:03:15 +0200

    2 files modified: Working UsersList + User

--- src/components/User.js
+++ src/components/User.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaTrash } from 'react-icons/fa';
 import { useUsers } from './UserProvider';
 
 export default function User({ id, name, email, firstname, lastname }) {
@@ -8,7 +9,7 @@ export default function User({ id, name, email, firstname, lastname }) {
     <div>
       <h1 style={{ display: 'inline' }}>{name}</h1>{' '}
       <button style={{ display: 'inline' }} onClick={(x) => removeUser(id)}>
-        {/* <FaTrash /> */}X
+        <FaTrash />
       </button>
       <div style={{ clear: 'both' }} />
       {firstname} {lastname} <br />
