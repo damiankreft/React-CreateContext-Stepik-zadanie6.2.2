61a1e5ebf95aba4ff6e16e7594696998e0688fab
damiankreft <25139693+damiankreft@users.noreply.github.com>
2023-06-17 16:19:03 +0200

    2 files modified

--- src/components/User.js
+++ src/components/User.js
@@ -3,19 +3,17 @@ import FaTrash from 'react-icons/fa';
 import { useUsers } from './UserProvider';
 
 export default function User({ id, name, email, firstname, lastname }) {
-  const { removeUser } = useUsers();
+  const { users, removeUser } = useUsers();
 
   return (
-    <section>
-      <h1>{name}</h1>
-      <p>
-        {firstname} {lastname} <br />
-        {email} <br />
-        {id}
-      </p>
-      <button onClick={() => removeUser(id)}>
-        <FaTrash />
+    <div>
+      <h1 style={{ display: 'inline' }}>{name}</h1>{' '}
+      <button style={{ display: 'inline' }} onClick={(x) => removeUser(id)}>
+        {/* <FaTrash /> */}X
       </button>
-    </section>
+      <div style={{ clear: 'both' }} />
+      {firstname} {lastname} <br />
+      {email} <br />
+    </div>
   );
 }
--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import User from './User';
+import User from './User.js';
 import { useUsers } from './UserProvider.js';
 
 export default function UsersList() {
   const { users } = useUsers();
 
   console.log(users);
-  users.map((x) => {
-    // console.log(x);
-  });
   if (!users.length) {
     return <div>No users listed.</div>;
   }
 
   return (
     <div>
-      test
-      {users.map((u) => {
-        console.log(u);
-        <User key={u.id} {...u} />;
-      })}
+      {users.map((user) => (
+        <User key={user.id} {...user} />
+      ))}
     </div>
   );
 }
